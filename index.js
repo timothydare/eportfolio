727490e286fbdf6d0c14d75650ababbb6000252c
Timothy <tedare115@gmail.com>
2022-08-15 19:54:59 -0700

    modal and dark theme

---
+++ index.js
@@ -0,0 +1,40 @@
+let isModalOpen = false;
+let contrastToggle = false;
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if(contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+function contact (event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_psfbhit',
+            'template_m8dvm3q',
+            event.target,
+            'BefuySNEcAaWbtQUg'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on email.email.com"
+        )
+    })
+}
+function toggleModal () {
+    if(isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal__open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal__open";
+}
